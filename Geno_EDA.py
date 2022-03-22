d68bf5fd7a9fc4bd2d2ea584256c57fc3c3bd180
ELI Kuo <101239889+Cereal-Kuo@users.noreply.github.com>
2022-03-22 10:50:15 +0800

    Update Geno_EDA.py

--- Geno_EDA.py
+++ Geno_EDA.py
@@ -1,4 +1,5 @@
 """
+資料格式應用
 Basic Packages for further usage
 """
 # Basic
@@ -9,3 +10,27 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import seaborn as sns
+
+"""
+資料處理
+Data Proccessing
+"""
+
+# Importing the dataset
+train_df = pd.read_csv('train.csv')
+test_df = pd.read_csv('test.csv')
+
+# Dropped the duplicate
+train_df.drop(columns=["row_id"], inplace=True)
+y = train_df['target']
+X = train_df.drop(columns=['target'])
+
+# Label Encoder
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+y = encoder.fit_transform(y)
+
+# Dataset Spliting for training/ testing set
+from sklearn.model_selection import train_test_split
+X_train, X_valid , y_train , y_valid = train_test_split(X, y, test_size = 0.2, random_state = 42)
+
